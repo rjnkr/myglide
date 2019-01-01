e7f38918cb94a8d91d16acfa6d3c149a687f0d84
rjnkr <richard@jnkr.eu>
2019-01-01 17:09:53 +0100

    added tabs to vliegtuig_logboek_scherm.dart

--- my_glide/lib/pages/vliegtuig_logboek_scherm.dart
+++ my_glide/lib/pages/vliegtuig_logboek_scherm.dart
@@ -12,24 +12,56 @@ class _VliegtuigLogboekScreenState extends State<VliegtuigLogboekScreen> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: MyGlideConst.appBarBackground(),
-        iconTheme: IconThemeData(color: MyGlideConst.frontColor),
-        title: Text(
-          "Vliegtuig logboek",
-          style: MyGlideConst.appBarTextColor()
+    return DefaultTabController(
+      length: 3,
+      child: 
+        Scaffold(
+          appBar: AppBar(
+            backgroundColor: MyGlideConst.appBarBackground(),
+            iconTheme: IconThemeData(color: MyGlideConst.frontColor),
+            title: Text(
+              "Vliegtuig logboek",
+              style: MyGlideConst.appBarTextColor()
+            ),
+            bottom: TabBar(
+              tabs:  _toonTabbladen(),
+              labelStyle: TextStyle(fontSize: MyGlideConst.labelSizeLarge),
+              labelColor: MyGlideConst.frontColor,
+              indicatorColor: MyGlideConst.frontColor,
+              isScrollable: true,
+              
+            ),
+            actions: <Widget>[
+              IconButton (
+            //    onPressed: () => _ophalenLogboek(true),
+                icon: Icon(Icons.refresh, color: MyGlideConst.frontColor),
+                padding: const EdgeInsets.only(right: 10.0)              
+              )
+            ],
+          ),
+          drawer: HoofdMenu(),
+         body: TabBarView(
+           children: <Widget>[
+             Icon(Icons.directions_car),
+             Icon(Icons.directions_bike),
+             Icon(Icons.directions_bus),
+             Icon(Icons.directions_bus),
+           ],
+         )
         ),
-        actions: <Widget>[
-          IconButton (
-        //    onPressed: () => _ophalenLogboek(true),
-            icon: Icon(Icons.refresh, color: MyGlideConst.frontColor),
-            padding: const EdgeInsets.only(right: 10.0)              
-          )
-        ],
-      ),
-      drawer: HoofdMenu(),
-     // body: 
     );
+  }
+
+  List<Widget> _toonTabbladen(){
+    List<Widget> retVal = List<Widget>();
+
+    for (int i=1; i< 10; i++) {
+      retVal.add(_toonTabblad("E$i"));
+    }
+    return retVal;
+  }
+
+  Widget _toonTabblad(String callsign) {
+    return Text(callsign);
   }
 }
